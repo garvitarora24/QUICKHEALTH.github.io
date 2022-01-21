5112c7b54884271f20640cd961d94a2f785fa72b
garvit arora <96050366+garvitarora24@users.noreply.github.com>
2022-01-21 17:25:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("demo").onmouseover = function() {mouseOver()};
+document.getElementById("demo").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("demo").src="chest.png";
+}
+
+function mouseOut() {
+  document.getElementById("demo").src="chest.png";
+}
